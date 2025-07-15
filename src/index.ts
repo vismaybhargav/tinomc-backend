21ce900bf4e6cae4a27d51bc10586dbd2cb98307
Vismay Bhargav <vismay.bhargav@gmail.com>
2025-07-15 05:41:21 -0700

    feat: update settings and api routes

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,62 @@
 import { Hono } from 'hono'
 import { Rcon, Game } from 'rcon-node'
 import { cors } from 'hono/cors';
-const { RCON_HOST, RCON_PASSWORD } = process.env
+const { RCON_HOST, RCON_PASSWORD, RCON_PORT } = process.env
 
-const RCON_PORT = process.env.RCON_PORT ? Number(process.env.RCON_PORT) : 25575;
-
-if (!RCON_HOST || !RCON_PASSWORD) {
-  throw new Error('Missing RCON_* env vars')
+if (!RCON_HOST || !RCON_PASSWORD || !RCON_PORT) {
+  throw new Error('Missing RCON_* env vars');
 }
 
-const app = new Hono()
+const app = new Hono();
+
+const rconConnection = await Rcon.connect({
+  host: RCON_HOST,
+  port: RCON_PORT ? parseInt(RCON_PORT) : 25575,
+  password: RCON_PASSWORD,
+  game: Game.MINECRAFT,
+});
 
 app.use("*", cors({
   origin: "*",// process.env.NODE_ENV === "dev" ? 'http://localhost:3000' : "http://vercel.com",       // or '*' for public APIs
   allowMethods: ['GET', 'POST'],
   allowHeaders: ['Content-Type', 'Authorization'],
   credentials: true
-}))
+}));
 
-app.get('/api', async (c) => {
-  return c.text("OK");
-})
+app.get('/api', (c) => {
+  return c.text("STATUS: OK");
+});
+
+app.get('api/server-status', async (c) => {
+  const ac = new AbortController();
+
+   try {
+    const res = await fetch("http://192.168.6.186:25565", {
+      method: "HEAD",
+      cache: "no-cache",
+      signal: ac.signal
+    });
+    
+    res.ok ? c.json({ status: "online" }) : c.json({ status: "offline" });
+   }
+});
 
 app.post('/api/rcon', async (c) => {
-  const { command } = await c.req.json()
+  const { command } = await c.req.json();
+
   if (typeof command !== 'string') {
-    return c.json({ error: '`command` must be a string' }, 400)
+    return c.json({ error: '`command` must be a string' }, 400);
   }
 
-  const rconConnection = await Rcon.connect({
-    host: RCON_HOST,
-    port: RCON_PORT,
-    password: RCON_PASSWORD,
-    game: Game.MINECRAFT,
-  })
-
-  const reply = await rconConnection.send(command)
+  const reply = await rconConnection.send(command);
 
   rconConnection.end();
 
-  return c.json({ ok: true, reply })
+  return c.json({ ok: true, reply });
+});
+
+process.on("exit", (code) => {
+  rconConnection.end();
 })
 
 /**
